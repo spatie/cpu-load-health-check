c8f455e9e205c29788f2107368bbd61205a32bc1
freek <freek@spatie.be>
2021-12-13 11:21:52 +0100

    wip

--- src/CpuLoadCheck.php
+++ src/CpuLoadCheck.php
@@ -8,6 +8,10 @@ class CpuLoadCheck
 {
     public static function measure(): self
     {
+        if (! function_exists('sys_getloadavg')) {
+            throw CouldNotMeasureCpuLoad::make();
+        }
+
         $result = sys_getloadavg();
 
         if (! $result) {
