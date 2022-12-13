e4964fdc239543857d2f2418f49e59599b88e4fa
Sergey Kudashev <kudashevs@gmail.com>
2022-12-13 17:37:09 +0300

    Update tests add skipping of the can measure cpu load test on Windows platforms.

--- tests/CpuLoadTest.php
+++ tests/CpuLoadTest.php
@@ -7,4 +7,9 @@ it('can measure cpu load', function () {
        ->lastMinute->toBeGreaterThan(0)
        ->last5Minutes->toBeGreaterThan(0)
        ->last15Minutes->toBeGreaterThan(0);
-});
+})->skip(fn () => isWindows() === true, 'This feature is not supported on Windows platforms.');
+
+function isWindows(): bool
+{
+    return str_starts_with(strtoupper(PHP_OS), 'WIN');
+}
